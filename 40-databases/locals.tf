781b707ef76b3d47bde2c8d5d1965a74b4fdc956
Praveen1221-TF <praveensunnam123@gmail.com>
2025-10-31 13:45:21 +0530

    terraform

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  ami_id =data.aws_ami.Joindevops
+  ami_id =data.aws_ami.Joindevops.id
   common_name_suffix = "${var.project_name}-${var.environment}" # roboshop-dev
   mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
   database_subnet_id = split("," , data.aws_ssm_parameter.database_subnet_ids.value)[0]
