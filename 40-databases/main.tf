40fb376d794c45727a5f62eb39c8cf0c87edd621
Praveen1221-TF <praveensunnam123@gmail.com>
2025-10-31 13:38:05 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -22,7 +22,7 @@ resource "terraform_data" "mongodb" {
       type = "ssh"
       user = "ec2-user"
       password = "DevOps321"
-      host = aws.instance.mongodb.private_ip
+      host = aws_instance.mongodb.private_ip
   }
 
 
