24a8b2a6920d30e8428c0661854343bb22c41291
Praveen1221-TF <praveensunnam123@gmail.com>
2025-11-15 14:50:34 +0530

    terraform ansible

--- 60-catalogue/catalogue.sh
+++ 60-catalogue/catalogue.sh
@@ -4,7 +4,7 @@ component=$1
 environment=$2
 dnf install ansible -y
 
-REPO_URL=https://github.com/daws-86s/ansible-roboshop-roles-tf.git
+REPO_URL=https://github.com/Praveen1221-TF/ansible-roboshop-tf.git
 REPO_DIR=/opt/roboshop/ansible
 ANSIBLE_DIR=ansible-roboshop-roles-tf
 
